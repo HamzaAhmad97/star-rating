38f3cb2efdc4c674b81dafed9cb9aa2ace9d461f
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-08-07 18:44:56 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import StarRating from './StarRating';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StarRating />
   </React.StrictMode>,
   document.getElementById('root')
 );
